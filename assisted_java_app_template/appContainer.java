53c1a15c921845d2ed2c963f8dcc038bcb5649a6
Matthew Papesh <Papesh.m2@student.jths.org>
2021-09-05 15:50:00 -0500

    added notes

--- assisted_java_app_template/appContainer.java
+++ assisted_java_app_template/appContainer.java
@@ -10,7 +10,24 @@ import lower_level.app;
     
     NOTE: You can do really anything in this file, but just don't change anything inside of the
     app_container() function!
+
+    "BUILT_IN FUNCTIONS":
+    
+    - createSprite(String sprite_name, String file_name) -> creates a sprite given the file name from the sprites folder.
+    - deleteSprite(String sprite_name) 
+    - displaySprite(String sprite_name, int x, int y) -> displays a sprite a specified coordinates, but it may or may 
+                                                           not be visible depending on the sprite's visibility status.
+     
+    - toggleSpriteVisibility(String sprite_name, boolean visible) -> visible = true shows a sprite, and visible = false
+                                                                       hides the sprite.
+     
+    Both functions below are to get the input from your computer's keyboard, but what it returns is an integer and 
+    not a character given that not every key has a character.  
+     
+    - getKeyPressed()
+    - getKeyReleased()
 */
+    
 
 // All code should be written in here:
 public class appContainer extends app 
@@ -23,7 +40,7 @@ public class appContainer extends app
     public void execute()
     {
         // Write code here: 
-        
+
     }
 
 }
