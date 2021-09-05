04d55e47d292634c5f58fdd65125ecc17f30e5a0
Matthew Papesh <Papesh.m2@student.jths.org>
2021-09-04 21:52:22 -0500

    Can now get user input

---
+++ assisted_java_app_template/appContainer.java
@@ -0,0 +1,28 @@
+import lower_level.app;
+
+/* 
+    This is where your app should be created. ALl files relavent to your app go in the "higher_level" folder, 
+    with any images going into the "sprites" folder. 
+
+    When programming, there will always be a main() function; it's what is called(ran) by the operating system 
+    so your computer knows where to start in terms of executing your code. However, this time you will be 
+    treating the "execute()" function as the "main()" or "public static void main(String[] args)" function. 
+    
+    NOTE: You can do really anything in this file, but just don't change anything inside of the
+    app_container() function!
+*/
+
+// All code should be written in here:
+public class appContainer extends app 
+{ 
+    public appContainer()
+    {
+        super();
+    }
+
+    public void execute()
+    {
+        // Write code here: 
+    }
+
+}
