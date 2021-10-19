e07d8384bcde217d0822dcf2adc009b7050b1ea8
StrongaCyborgs24 <89878096+StrongaCyborgs24@users.noreply.github.com>
2021-10-19 17:25:42 -0500

    Additions

--- assisted_java_app/app_projects/Aaron/Main.java
+++ assisted_java_app/app_projects/Aaron/Main.java
@@ -5,7 +5,36 @@ public class Main
     public static void main(String[] args)
     {
         container.execute();
+        let leftShip;
+        let righSship;
+        
+        function setup() {
+        	createCanvas(200, 200);
+        	leftShip = new Ship(width * 0.33);
+        	rightShip = new Ship(width * 0.66);
+        }
+        function draw() {
+        	background(0);
+        	
+        	leftShip.update();
+        	rightShip.update();
+        	
+        	leftShip.display();
+        	rightShip.display();
+        }
+        function keyPressed() {
+        	if (keycode == UP_ARROW) {
+        		rightShip.isUp = true;
+        		rightShip.isDown = false;
+        	} else if (keycode == DOWN_ARROW);
+        		rightship.isDown = true;
+        		rightShip.isUp = false; 
+        		
+        	}
+        
+   
     }
+    
 }
 
 
